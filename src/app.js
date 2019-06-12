9bdc44ae06948d9531f9980bd3229f0036c97575
Naveed Raza <naveedraza@gmail.com>
2019-06-11 22:44:54 -0700

    Buid-it-visibility app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,78 @@
-class VisibilityToggle extends React.Component{
-    constructor(props){
-        super(props)
-        this.handleToggleVisibility=this.handleToggleVisibility.bind(this);
-        this.state={
-            visibility: true
-        }
-    }
-    handleToggleVisibility(){
-this.setState((prevState)=>{return {visibility: !prevState.visibility}} )
-    }
+/* Props  : Passing value to the components
+We pass props by setting key value pair as we do with html, like id, class,etc.
+Key can be any thing we like, title= "Indecision App"
+we can access these props(key values) as we do for normal classes by using "this." + "props" i.e. this.props.key
+
+*/ 
+//Creating React Component Class
+
+//Notes: for React Components we need uppercase letter to define class.
+//React uses Uppercase letter to differentiate between html tags and reacts components
+class IndecisionApp extends React.Component{
+    
     render(){
+        const title= "Indecision App"
+        const subtitle= "Put your life in hands of computer"
+        const options=["Thing One","Thing Two","Thing Four"]
+        // title={title} we passing javascript expression : string to make it mor flexible.
+        // No commas "," when we are passing two or more props e.g. <Header title={title} subtitle={subtitle}  /> 
         return <div>
-        <h1>Visibility Toggle</h1>
-            <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details' }</button>
-            {this.state.visibility && (<div><p>Hey! these are some details</p></div>)}
+                <Header title={title} subtitle={subtitle}  /> 
+                <Action />
+                <Options options={options}/>
+                <AddOption />
             </div>
     }
 }
-ReactDOM.render(<VisibilityToggle />,document.getElementById('app'));
+
+class Header extends React.Component{  // we extend React.Component class to create REACT COMPONENT
+    render(){ // REACT COMPONENT needs render function 
+        // console.log(this.props); // this is how we dump object values on screen
+        // Writing screen string in jsx we use parenthisis as we did for title 👇🏻
+        return <div>
+        <h1>{this.props.title}</h1> 
+        <h2>{this.props.subtitle}</h2>
+        
+        </div> // render returns JSX,this is compulsory part of React Component.
+
+
+    }
+}
+class Action extends React.Component{
+    render(){
+        return <button>What Should I do?</button>
+    }
+}
+class Options extends React.Component{
+    render(){
+        return <div>
+        {/*this.props.options.length*/} {/*If we are creating an array of JSX as below for option component we need key props e.g.👇🏻, but key isn't going to be available in component, it is a special reserve word  */} 
+        {this.props.options.map((option)=> <Option key={option} optionText={option}/>)}{/* We dont use curly braces, otherwise it wouldnt work*/}
+
+        <Option option={"option"}/> 
+        <Option />
+        </div>
+    }
+}
+class Option extends React.Component{
+    render(){
+        return <div>{this.props.optionText}</div>
+    }
+}
+class AddOption extends React.Component{
+    render(){
+        return <div>AddOption component here</div>
+    }
+}
+/*const jsx=(
+    <div>
+    <Header /> 
+    <Action />
+    <Options />
+    <AddOption />
+    </div>);
+
+*/
+// ReactDOM.render(jsx,document.getElementById('app')) // Render React component we use this 
+// 👆🏻 Instead of JSX we can provide direct react component like this 👇🏻
+ReactDOM.render(<IndecisionApp />,document.getElementById('app')) // Render React component we use this 
--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -20,3 +20,25 @@ ReactDOM.render(templateOne,appRoot)
 };
 
 visibilityApp();
+
+// class VisibilityToggle extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.handleToggleVisibility=this.handleToggleVisibility.bind(this);
+//         this.state={
+//             visibility: true
+//         }
+//     }
+//     handleToggleVisibility(){
+// this.setState((prevState)=>{return {visibility: !prevState.visibility}} )
+//     }
+//     render(){
+//         return <div>
+//         <h1>Visibility Toggle</h1>
+//             <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details' }</button>
+//             {this.state.visibility && (<div><p>Hey! these are some details</p></div>)}
+//             </div>
+//     }
+// }
+// ReactDOM.render(<VisibilityToggle />,document.getElementById('app'));
+
